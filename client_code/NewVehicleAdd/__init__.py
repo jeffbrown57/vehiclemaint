4bba8facd003aa3d90f72af3a227e180389464a4
jbrow57@gmail.com <jbrow57@gmail.com>
2022-06-12 12:14:43 +0000

    Edited settings, standard-page.html and NewVehicleAdd

--- client_code/NewVehicleAdd/__init__.py
+++ client_code/NewVehicleAdd/__init__.py
@@ -8,10 +8,12 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
+from anvil.js.window import retGitJS
+
 class NewVehicleAdd(NewVehicleAddTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
-    self.label_2.text = anvil.js('getGitJS')
+    self.label_2.text = retGitJS()
